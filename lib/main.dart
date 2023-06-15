896a5ae073f6ee476ad5585c9acf43ec7ea88691
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-06-15 11:16:21 +0530

    imported home , loading , location

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:la_brise/Activity/home.dart';
+import 'package:la_brise/Activity/loading.dart';
+import 'package:la_brise/Activity/location.dart';
 
 void main() {
   runApp(MaterialApp(
