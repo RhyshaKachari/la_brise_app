3c4bbb8c5e41ff9741c2ac11b46e4e155d42bfa7
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-06-19 09:06:33 +0530

    api hidden

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:la_brise/Activity/home.dart';
 import 'package:la_brise/Activity/loading.dart';
 import 'package:la_brise/Activity/location.dart';
+import 'package:flutter_config/flutter_config.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
+ runApp(MaterialApp(
     routes: {
       "/" : (context) => Loading(),
       "/home" : (context) => Home(),
