c7fd034900098e22adce18edca71229667630c61
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-06-20 13:06:24 +0530

    made UI for loading screen

--- lib/Activity/loading.dart
+++ lib/Activity/loading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:la_brise/Worker/worker.dart';
-
+import 'package:flutter_spinkit/src/circle.dart';
+import 'package:google_fonts/google_fonts.dart';
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
 
@@ -9,7 +10,6 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
   late String temp;
   late String hum ;
   late String air_speed ;
@@ -25,14 +25,16 @@ class _LoadingState extends State<Loading> {
     air_speed = instance.air_speed ;
     des = instance.description;
     main = instance.main ;
+   Future.delayed(Duration(seconds: 2), () {
+     Navigator.pushReplacementNamed(context, '/home' , arguments: {
+       "temp_value" : temp,
+       "hum_value" : hum,
+       "air_speed_value" : air_speed ,
+       "des_value" : des ,
+       "main_value" : main
+     });
+   });
 
-    Navigator.pushReplacementNamed(context, '/home' , arguments: {
-      "temp_value" : temp,
-      "hum_value" : hum,
-      "air_speed_value" : air_speed ,
-      "des_value" : des ,
-      "main_value" : main
-    });
 
   }
   @override
@@ -44,9 +46,26 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text("Loading"),
+      body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset("images/newicon.webp",height: 240 ,width: 240, ),
+          SizedBox(height: 30,),
+          Text("Weather App", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500 ,color: Colors.white),),
+         SizedBox(height: 15,),
+          Text("Made by Rhysha",
+          style:  GoogleFonts.lato(
+            fontSize: 18,color: Colors.white
+          ),),
+        SizedBox(height: 30,),
+        SpinKitCircle(
+          color: Colors.white,
+          size: 50.0),
+        ],
       ),
+      ),
+      backgroundColor: Colors.black,
     );
   }
 }
