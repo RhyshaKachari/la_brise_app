3546ddea9df80dc8f26b5b16aabbb6270c670499
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-06-20 08:42:59 +0530

    error handling done

--- lib/Worker/worker.dart
+++ lib/Worker/worker.dart
@@ -19,31 +19,42 @@ class worker{
 //method
 
   Future<void> getData() async{
-    String api_mine = await FlutterConfig.get('API_ID');
-    // http.Response response = await http.get(Uri.parse(api_url));
-    var url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$location&appid=$api_mine");
-    http.Response response = await http.get(url);
-    Map data =  jsonDecode(response.body);
+    try{
+      String api_mine = await FlutterConfig.get('API_ID');
+      // http.Response response = await http.get(Uri.parse(api_url));
+      var url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$location&appid=$api_mine");
+      http.Response response = await http.get(url);
+      Map data =  jsonDecode(response.body);
 
-    //Getting Temp Humidity
-    Map temp_data = data['main'];
-    String getHumidity = (temp_data['humidity']).toString();
-    String getTemp = (temp_data['temp'] -273.15).toString();
+      //Getting Temp Humidity
+      Map temp_data = data['main'];
+      String getHumidity = (temp_data['humidity']).toString();
+      String getTemp = (temp_data['temp'] -273.15).toString();
 
-    //Getting air speed
-    Map wind = data['wind'];
-    String getAir_speed = (wind["speed"]*3.6).toString();
+      //Getting air speed
+      Map wind = data['wind'];
+      String getAir_speed = (wind["speed"]*3.6).toString();
 
-    //Getting Description
-    List weather_data = data['weather'];
-    Map weather_main_data = weather_data[0];
-    String getMain_des = weather_main_data['main'];
-    String getDesc = weather_main_data["description"];
+      //Getting Description
+      List weather_data = data['weather'];
+      Map weather_main_data = weather_data[0];
+      String getMain_des = weather_main_data['main'];
+      String getDesc = weather_main_data["description"];
 
-    //Assigning Values
-    temp = getTemp;
-    humidity = getHumidity;
-    air_speed = getAir_speed;
+      //Assigning Values
+      temp = getTemp;
+      humidity = getHumidity;
+      air_speed = getAir_speed;
+      description = getDesc ;
+      main = getMain_des ;
+    }
+    catch(e){
+      temp = "Can't Find Data";
+      humidity = "Can't Find Data";
+      air_speed = "Can't Find Data";
+      description = "Can't Find Data" ;
+      main = "Can't Find Data" ;
+    }
 
   }
 }
