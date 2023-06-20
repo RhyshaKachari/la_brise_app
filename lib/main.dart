8d3c3870f0d44abe7f5c77bb6f8effa9989330ec
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-06-20 13:11:18 +0530

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterConfig.loadEnvVariables();
  runApp(MaterialApp(
+   debugShowCheckedModeBanner: false,
     routes: {
       "/" : (context) => Loading(),
       "/home" : (context) => Home(),
